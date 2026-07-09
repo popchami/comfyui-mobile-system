f88d75dd18c9f2f3b1aef82f3bb96600199400fc
popchami <47472197+popchami@users.noreply.github.com>
2026-07-09 09:35:57 +0900

    Add generated image model

---
+++ mobile-app/flutter_mvp/lib/models/generated_image.dart
@@ -0,0 +1,13 @@
+class GeneratedImage {
+  const GeneratedImage({
+    required this.filename,
+    required this.subfolder,
+    required this.type,
+    required this.url,
+  });
+
+  final String filename;
+  final String subfolder;
+  final String type;
+  final Uri url;
+}
